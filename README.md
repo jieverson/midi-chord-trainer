53ae194e6ad3477af6f63292fc04b412479ea22e
Jiéverson Maissiat <jieverson@gmail.com>
2017-03-24 18:08:05 -0300

    add watchify; rebundle on file changes

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 npm install
 gulp
 ```
+
+### Release:
+```
+gulp release
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,45 @@
 var gulp = require('gulp')
+var watchify = require('watchify')
 var browserify = require('browserify')
 var uglify = require('gulp-uglify')
 var source = require('vinyl-source-stream')
 var buffer = require('vinyl-buffer')
 var babel = require('gulp-babel')
 
-gulp.task('default', function () {
-    return browserify('./src/app.js', { debug: false }).bundle()
+function bundle(watching, release){
+    var bundling = 
+        watching
+        .bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+    
+    if(release){
+        bundling = bundling.pipe(
+            babel({
+                presets: ['es2015']
+            }))
         .pipe(uglify())
-        .pipe(gulp.dest('./'))
+    }
+
+    return bundling.pipe(gulp.dest('./'))
+}
+
+gulp.task('default', function () {
+     var watching = watchify(browserify(
+        './src/app.js', { 
+            debug: true
+        }))
+    watching.on('update', () => bundle(watching))
+
+    return bundle(watching)
 })
+
+gulp.task('release', function () {
+    var watching = browserify(
+        './src/app.js', { 
+            debug: true
+        })
+
+    return bundle(watching, true)
+})
+
