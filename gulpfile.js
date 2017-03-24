9dd3acc4756dd804747b62ee63129a1578ea8804
Jiéverson Maissiat <jieverson@gmail.com>
2017-03-24 16:45:54 -0300

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp')
 var browserify = require('browserify')
-var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
 var babel = require('gulp-babel')
 
 gulp.task('default', function () {
-    return browserify('./src/app.js').bundle()
+    return browserify('./src/app.js', { debug: false }).bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./'));
+        .pipe(buffer())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./'))
 })
